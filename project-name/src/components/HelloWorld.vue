f6013b75048d63457e97a047c313facbae0d9a23
17638133619 <3142095701@qq.com>
2019-11-26 23:49:16 +0800

    dl-1

---
+++ project-name/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>111</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
+<<style>
+  div{
+    width:100%;
+    heigth:2rem;
+    font-size:0.3rem;
+  }
+</style>
